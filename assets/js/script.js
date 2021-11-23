ae449cf076e9d4924b6866f3d6f0377f22101381
Victor Pereira <victorpereirademelo18@gmail.com>
2021-11-23 18:29:08 -0300

    ViaCEP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
 // Inicio do Programa
 
-// Limpando valores do formulário do cep
+// Limpa valores do formulário do cep
 function limpa_formulário_cep() {
-    document.getElementById('rua').value = ("");
-    document.getElementById('bairro').value = ("");
-    document.getElementById('cidade').value = ("");
-    document.getElementById('uf').value = ("");
-    document.getElementById('ibge').value = ("");
+    document.getElementById('rua').value = "";
+    document.getElementById('bairro').value = "";
+    document.getElementById('cidade').value = "";
+    document.getElementById('uf').value = "";
+    document.getElementById('ibge').value = "";
 }
 
+// Atualiza os campos com os valores
 function meu_callback(conteudo) {
-
+    if (!("erro" in conteudo)) {
+        document.getElementById('rua').value = (conteudo.logradouro);
+        document.getElementById('bairro').value = (conteudo.bairro);
+        document.getElementById('cidade').value = (conteudo.localidade);
+        document.getElementById('uf').value = (conteudo.uf);
+        document.getElementById('ibge').value = (conteudo.ibge);
+    } else {
+        // Cep não encontrado
+        limpa_formulário_cep();
+        alert("CEP não encontrado");
+    }
 }
+
+
