e4c54b93c2f62e7a7fd887b5adc15c886267f99e
Victor Pereira <victorpereirademelo18@gmail.com>
2021-11-23 17:07:14 -0300

    ViaCEP

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Inicio do Programa
+
+// Limpando valores do formulário do cep
+function limpa_formulário_cep() {
+    document.getElementById('rua').value = ("");
+    document.getElementById('bairro').value = ("");
+    document.getElementById('cidade').value = ("");
+    document.getElementById('uf').value = ("");
+    document.getElementById('ibge').value = ("");
+}
+
+function meu_callback(conteudo) {
+
+}
